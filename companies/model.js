f123cdfd22442122c9eda423ac16051297b21e47
Hoan Phung <hoanphung.ptso1@gmail.com>
2018-11-14 11:06:37 +0100

    companies created

---
+++ companies/model.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const Company = sequelize.define('companies', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'name',
+    allowNull: false
+  },
+  foundingYear: {
+    type: Sequelize.INTEGER,
+    field: 'founding_year',
+    allowNull: false
+  },
+  bio: {
+    type: Sequelize.STRING
+  },  
+}, {
+  timestamps: false,
+  tableName: 'companies'
+})
+
+module.exports = Company
